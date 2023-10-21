64bdb053d8e30b02879ecb6ae7e20bb20d3fb20d
jshohet <parykeeth90@gmail.com>
2023-10-21 12:04:08 -0400

    ...

--- app/dist/page.js
+++ app/dist/page.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+// exports.__esModule = true;
 var Header_1 = require("../components/Header");
 function Home() {
     return (React.createElement("main", null,
